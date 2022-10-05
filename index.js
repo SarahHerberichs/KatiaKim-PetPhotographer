f570a090428c101d4bb83007f61fa6c6b407ddb6
sarahherberichs <sarah.herberichs@neuf.fr>
2022-10-05 16:43:34 +0200

    051022-2

--- index.js
+++ index.js
@@ -7,15 +7,15 @@ function mobileNavAnimation() {
   icon.addEventListener("click", function () {
     navBar.classList.toggle("burgerClicked");
     for (let i = 0; i < 4; i++) {
-      if (window.innerWidth < 880) {
-        dropDown[i].addEventListener("click", function () {
-          dropDown[i].classList.toggle("heightProgress");
-        });
-        if (!navBar.classList.contains("burgerClicked")) {
-          dropDown[i].classList.remove("heightProgress");
-        }
+      /*if (window.innerWidth < 880) {*/
+      dropDown[i].addEventListener("click", function () {
+        dropDown[i].classList.toggle("heightProgress");
+      });
+      if (!navBar.classList.contains("burgerClicked")) {
+        dropDown[i].classList.remove("heightProgress");
       }
     }
+    // }
   });
 }
 mobileNavAnimation();
