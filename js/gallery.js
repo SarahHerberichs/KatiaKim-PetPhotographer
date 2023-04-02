0d47fb1242036a9c4a46836d7c1d8f85a3215be4
Herberichs <94360739+crepechoco57@users.noreply.github.com>
2023-04-02 17:06:21 +0200

    first commit

--- js/gallery.js
+++ js/gallery.js
@@ -10,24 +10,23 @@ function viewGridPhotos() {
     let nthDivAdminPhotos = document.querySelectorAll(".nthDivAdminPhotos");
     let nthArticleDivPhotos = document.querySelectorAll(".nthArticleDivPhotos");
     let buttonClose = document.querySelectorAll(".buttonCloseGallery");
-    let chevronTop = document.querySelector(".chevronUp")
+    let chevronTop = document.getElementsByClassName("fa-circle-chevron-up");
 
     for (let i = 0; i < nthArticleDivTitle.length; i++) {
-      
       nthArticleDivTitle[i].addEventListener("click", function () {
         nthArticleDivTitle[i].classList.add("invisible");
         nthArticleDivPhotos[i].classList.add("gridVisible");
         nthDivAdminPhotos[i].classList.add("invisible");
         buttonClose[i].classList.remove("invisible");
       });
-      
+
       buttonClose[i].addEventListener("click", function () {
         nthArticleDivTitle[i].classList.remove("invisible");
         nthArticleDivPhotos[i].classList.remove("gridVisible");
         nthDivAdminPhotos[i].classList.remove("invisible");
         buttonClose[i].classList.add("invisible");
-      });          
-      chevronTop.addEventListener("click", function () {
+      });
+      chevronTop[i].addEventListener("click", function () {
         nthArticleDivTitle[i].classList.remove("invisible");
         nthArticleDivPhotos[i].classList.remove("gridVisible");
         nthDivAdminPhotos[i].classList.remove("invisible");
@@ -38,4 +37,3 @@ function viewGridPhotos() {
   articles.forEach(playWithVisibility);
 }
 viewGridPhotos();
-
